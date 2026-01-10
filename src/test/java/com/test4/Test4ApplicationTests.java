1ac0c4c9ec245d05c4e59f8542732fc9fd6b05bd
sardhar vinayaka <sardhar vinayaka>
2026-01-10 20:49:35 +0530

    First Commit

---
+++ src/test/java/com/test4/Test4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
