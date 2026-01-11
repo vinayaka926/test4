b15547f3ef7214120d211d02a1229842b058bb3c
sardhar vinayaka <vinayaksardhar112@gmail.com>
2026-01-11 15:15:43 +0530

    Update Test5Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
+		int x =10;
+		int y= 20;
+		char c='x';
 		SpringApplication.run(Test4Application.class, args);
 	}
 
